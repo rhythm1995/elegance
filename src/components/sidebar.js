6a47bacdb0fc589c83131599b4af97ba5275345c
bugzhang <flyhighdream@qq.com>
2018-02-01 23:23:24 +0800

    add Sidebar,drop react-svg

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,11 +1,13 @@
 // 左侧栏
 import React, { Component } from 'react';
-import {Row, Col, Checkbox, Button, Input} from 'antd';
+import {Row, Menu, Icon, Input} from 'antd';
 import ReactSVG from 'react-svg'
 
 import '../App.css';
 
 const Search = Input.Search;
+const SubMenu = Menu.SubMenu;
+const MenuItemGroup = Menu.ItemGroup;
 
 export default class Sidebar extends Component {
     render() {
@@ -18,15 +20,25 @@ export default class Sidebar extends Component {
                       onSearch={value => console.log(value)}
                   />
               </div>
-              <ul className="sidebar-list">
-                  <li className="list-group-item">
-                      <Row>
-                          {}
-                      </Row>
-                  </li>
-              </ul>
+                  <Menu
+                      onClick={this.handleClick}
+                      style={{ width: 200 }}
+                      defaultSelectedKeys={['1']}
+                      defaultOpenKeys={['sub1']}
+                      mode="inline"
+                  >
+                      <SubMenu key="sub1" title={<span><span>Navigation One</span></span>}>
+                          <MenuItemGroup key="g1" title="Item 1">
+                              <Menu.Item key="1">Option 1</Menu.Item>
+                              <Menu.Item key="2">Option 2</Menu.Item>
+                          </MenuItemGroup>
+                          <MenuItemGroup key="g2" title="Item 2">
+                              <Menu.Item key="3">Option 3</Menu.Item>
+                              <Menu.Item key="4">Option 4</Menu.Item>
+                          </MenuItemGroup>
+                      </SubMenu>
+                  </Menu>
           </div>
         );
     }
 }
-
